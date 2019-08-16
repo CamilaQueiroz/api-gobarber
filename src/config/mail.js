ac0765d1fafdfe01fd9c67dc06e4d511c7ed36f0
Camila <camilasvqueiroz@gmail.com>
2019-08-16 00:00:50 -0300

    environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: 'c5f22899497b35',
-    pass: 'c972da753d945a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PAST,
   },
   secure: false,
   default: {
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://8377acde47034a2286d763c6e0607d8f@sentry.io/1532184',
+  dsn: process.env.SENTRY_DNS,
 };
